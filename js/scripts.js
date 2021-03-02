ffe2ee76fabff13dc55a5c0c8d3cba5ae5141a2d
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-03-02 12:41:04 +0100

    comprobación vacio y comprobación sin espacios

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,9 @@ listaPalabras.addEventListener("click", (elemento) => {
   const palabraCopiar = elemento.target.cloneNode(true);
   listaResultado.append(palabraCopiar);
 });
+
+function comprobar(elemento) {
+  elemento.value === "" ? document.querySelector(".crear").disabled = true : document.querySelector(".crear").disabled = false;
+
+  elemento.value.search(" ") !== -1 ? document.querySelector(".crear").disabled = true : document.querySelector(".crear").disabled = false;
+}
