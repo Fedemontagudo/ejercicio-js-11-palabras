0dd4b3c48bb6a2b06effcd94f07a6111fecb7e43
Fede Montagud <fedemontagudo@gmail.com>
2021-03-02 12:43:32 +0100

    funcior contar palabras correcta

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@ const listaPalabras = document.querySelector(".lista-palabras");
 
 const listaResultado = document.querySelector(".resultado");
 
+const nPalabras = document.querySelector(".contador-palabras");
+
 listaPalabras.addEventListener("click", (elemento) => {
   const palabraCopiar = elemento.target.cloneNode(true);
   listaResultado.append(palabraCopiar);
-  contarPalabras(listaResultado);
+  const palabrasContadas = contarPalabras(listaResultado);
+  nPalabras.textContent = palabrasContadas;
 });
 
 const contarPalabras = (variable) => variable.childElementCount;
