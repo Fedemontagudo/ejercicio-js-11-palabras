011e4fefa32e89ba3eb1389c93b13d8efa4b0591
Santiago Arias <santyac27@gmail.com>
2021-03-02 17:32:15 +0100

    Se añade funcion primeraLetraMayus

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,9 @@ const nPalabras = document.querySelector(".contador-palabras");
 listaPalabras.addEventListener("click", (elemento) => {
   if (elemento.target !== listaPalabras) {
     const palabraCopiar = elemento.target.cloneNode(true);
+    if (listaResultado.children.length === 0) {
+      palabraCopiar.textContent = primeraLetraMayus(palabraCopiar.textContent);
+    }
     listaResultado.append(palabraCopiar);
     contarPalabras(listaResultado);
   }
@@ -14,6 +17,11 @@ listaPalabras.addEventListener("click", (elemento) => {
 
 listaResultado.addEventListener("click", (elementoResultado) => {
   if (elementoResultado.target !== listaResultado) {
+    if (listaResultado.children[0] === elementoResultado.target
+      && listaResultado.children.length > 1) {
+      const palabraConMayuscula = listaResultado.children[1];
+      palabraConMayuscula.textContent = primeraLetraMayus(palabraConMayuscula.textContent);
+    }
     elementoResultado.target.remove();
     contarPalabras(listaResultado);
   }
@@ -23,3 +31,5 @@ const contarPalabras = (variable) => {
   const palabrasContadas = variable.childElementCount;
   nPalabras.textContent = palabrasContadas;
 };
+
+const primeraLetraMayus = (palabra) => palabra.charAt(0).toUpperCase() + palabra.slice(1);
