bca14890e2c9f712d5d4bd2f6aced657ae05321d
Fede Montagud <fedemontagudo@gmail.com>
2021-03-02 20:51:39 +0100

    funcion numeroPalabras seteada

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,29 @@ const listaPalabras = document.querySelector(".lista-palabras");
 
 const listaResultado = document.querySelector(".resultado");
 
-const nPalabras = document.querySelector(".contador-palabras");
-
 const nCaracteres = document.querySelector(".contador-caracteres");
 
 listaPalabras.addEventListener("click", (elemento) => {
   if (elemento.target !== listaPalabras) {
     const palabraCopiar = elemento.target.cloneNode(true);
     listaResultado.append(palabraCopiar);
-    contarPalabras(listaResultado);
+    calcular(listaResultado);
   }
 });
 
 listaResultado.addEventListener("click", (elementoResultado) => {
   if (elementoResultado.target !== listaResultado) {
     elementoResultado.target.remove();
-    contarPalabras(listaResultado);
+    calcular(listaResultado);
   }
 });
 
-
-
 const calcular = () => {
-  const liListaResultado = document.querySelectorAll(".resultado > li")
-  for (const cosa of liListaResultado);
+  const numeroPalabras = document.querySelectorAll(".resultado > li").length;
+  document.querySelector(".contador-palabras").textContent = numeroPalabras;
 
-
-
+  const numeroCaracteres = document.querySelectorAll("resultado > li");
+  document.querySelectorAll(".contador-caracteres").textContent.length = numeroCaracteres;
+  console.log(numeroCaracteres);
+};
+calcular();
