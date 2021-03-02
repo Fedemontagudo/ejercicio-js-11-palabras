b31adff02412ed031d2173940281589bd4e6e480
Fede Montagud <fedemontagudo@gmail.com>
2021-03-02 12:28:42 +0100

    creada funcion contar palabras

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,7 @@ const listaResultado = document.querySelector(".resultado");
 listaPalabras.addEventListener("click", (elemento) => {
   const palabraCopiar = elemento.target.cloneNode(true);
   listaResultado.append(palabraCopiar);
+  contarPalabras(listaResultado);
 });
+
+const contarPalabras = (variable) => variable.childElementCount;
